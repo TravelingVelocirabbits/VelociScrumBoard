3b0ede28781b20877ce3a35948b0430805195fa0
Ken <keniwane@gmail.com>
2023-09-16 17:27:13 -0700

    do not pull

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,7 +5,7 @@ const userController = {};
 // add a user
 userController.addUser = (req, res, next) => {
   const { name } = req.body;
-  
+
   User.create({
     name,
   })
@@ -13,22 +13,55 @@ userController.addUser = (req, res, next) => {
       res.locals.newUser = data;
       return next();
     })
-    .catch(err => {
+    .catch((err) => {
       return next({
         log: `userController.addUser: Error ${err}`,
         message: {
-          err: 'Error occurred in userController.addUser. Check server logs'
+          err: 'Error occurred in userController.addUser. Check server logs',
         },
         status: 400,
       });
     });
 };
 
+// update a user
+// userController.updateUser = (req, res, next) => {
+//   const {name} = req.params;
+//   const updateUser = req.body;
+
+//   User.findOneAndUpdate(
+//     {name: name},
+//     {name: updateUser}
+//   )
+// };
 
 // delete a user
 userController.removeUser = (req, res, next) => {
-
+  const { name } = req.params;
+  User.deleteOne({ name: name })
+    .then((data) => {
+      if (!data) {
+        return next({
+          log: `userController.removeUser: ${name} was not found in the database`,
+          message: {
+            err: 'User not found',
+          },
+          status: 404,
+        });
+      } else {
+        res.locals.deletedUser = data;
+        return next();
+      }
+    })
+    .catch((err) => {
+      return next({
+        log: `userController.addUser: Error ${err}`,
+        message: {
+          err: 'Error occurred in userController.addUser. Check server logs',
+        },
+        status: 400,
+      });
+    });
 };
-
 
 module.exports = userController;
