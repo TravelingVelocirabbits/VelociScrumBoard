90479b1fce76da0babfa5f8c7f5c9621845c7b52
Ken <keniwane@gmail.com>
2023-09-16 17:25:24 -0700

    test

--- server/routes/Router.js
+++ server/routes/Router.js
@@ -1,50 +1,54 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 const router = express.Router();
 
 const categoryController = require('../controllers/categoryController');
 const userController = require('../controllers/userController');
 const taskController = require('../controllers/taskController');
 
-// Define middleware functions separately
-const addCategoryMiddleware = categoryController.addCategory;
-const removeCategoryMiddleware = categoryController.removeCategory;
-const editCategoryMiddleware = categoryController.editCategory;
-const addUserMiddleware = userController.addUser;
-const removeUserMiddleware = userController.removeUser;
-const addTaskMiddleware = taskController.addTask;
+console.log('IM IN THE ROUTE ROUTER RERROUOTUERE');
 
 //CATEGORY CONTROLLERS
-router.post('/createcategory', addCategoryMiddleware, (req, res) => {
+router.post('/createcategory', (req, res) => {
   console.log('finished creating category', res.locals.category);
   res.status(200).json(res.locals.category);
 });
 
-router.post('/removecategory', removeCategoryMiddleware, (req, res) => {
+router.delete('/removecategory', categoryController.removeCategory, (req, res) => {
   console.log('finished removing category', res.locals.category);
   res.status(200).json(res.locals.category);
 });
 
-router.put('/editcategory', editCategoryMiddleware, (req, res) => {
+router.put('/editcategory', categoryController.editCategory, (req, res) => {
   console.log('finished updating category', res.locals.category);
   res.status(200).json(res.locals.category);
 });
 
 // USER CONTROLLERS
-router.post('/createuser', addUserMiddleware, (req, res) => {
-  res.status(200).json(res.locals.category);
+router.post('/user', userController.addUser, (req, res) => {
+  console.log('added user', res.locals.newUser);
+  res.status(200).json(res.locals.newUser);
 });
 
-router.post('/removeuser', removeUserMiddleware, (req, res) => {
-  // Your route logic here
+router.delete('/removeuser/:name', userController.removeUser, (req, res) => {
+  console.log('removed user', res.locals.deletedUser);
+  res.status(200).json(res.locals.deletedUser);
 });
 
 // TASK CONTROLLERS
-router.post('/createtask', addTaskMiddleware, (req, res) => {
-  req.session.task = res.locals.task;
-  console.log('finished creating task ', req.session.task);
-  res.status(200).redirect('/update');
+router.post('/createtask', taskController.addTask, (req, res) => {
+  console.log('finished creating task', res.locals.task);
+  res.status(200).json(res.locals.task);
+});
+
+router.delete('/removetask', taskController.removeTask, (req, res) => {
+  console.log('finished removing task', res.locals.task);
+  res.status(200).json(res.locals.task);
+});
+
+router.put('/edittask', taskController.editTask, (req, res) => {
+  console.log('finished updating task', res.locals.task);
+  res.status(200).json(res.locals.task);
 });
 
 module.exports = router;
