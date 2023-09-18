938fa0afb6b2065a50b54dd8efac659d8cf46ed1
Simon Lin <linsimon95@gmail.com>
2023-09-18 15:41:36 -0400

    task component ashetics

--- client/components/Task.js
+++ client/components/Task.js
@@ -1,35 +1,32 @@
 import React, { useState } from 'react';
 import { Draggable } from 'react-beautiful-dnd';
 import Category from './Category';
+import '../stylesheets/app.css';
+
 
 export default function Task({ task, index, onTaskClick, onTaskRemove }) {
   return (
     <Draggable draggableId={String(task._id)} index={index}>
       {(provided, snapshot) => (
-        <div className='taskDisplay'>
-          <div
-            ref={provided.innerRef}
-            {...provided.draggableProps}
-            {...provided.dragHandleProps}
-            style={{
-              userSelect: 'none',
-              padding: 16,
-              margin: '0 0 8px 0',
-              minHeight: '50px',
-              backgroundColor: snapshot.isDragging ? '#263B4A' : '#456C86',
-              color: 'white',
-              flex: 1,
-              ...provided.draggableProps.style,
-            }}
-            onClick={() => onTaskClick(task)}
-          >
+        <div className='taskDisplay'
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          style={{
+            userSelect: 'none',
+            backgroundColor: snapshot.isDragging ? '#263B4A' : '#456C86',
+            ...provided.draggableProps.style,
+          }}
+        >
+          <div className='taskContent' onClick={() => onTaskClick(task)}>
             {task.Task_Name}
-          </div><div>
-            <button
-              className="taskButton"
-              onClick={() => onTaskRemove(task._id)}
-            >Delete</button>
           </div>
+          <button
+            className={`taskButton ${snapshot.isDragging ? 'dragged' : ''}`}
+            onClick={() => onTaskRemove(task._id)}
+          >Delete
+          </button>
+
         </div>
       )}
     </Draggable>
