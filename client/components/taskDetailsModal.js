5f61c044940caae31b6af1b1fecaab873a8b4409
Andrew <apwicker@gmail.com>
2023-09-19 17:54:07 -0700

    Re-Designing Edit Task Modal Functionality

--- client/components/taskDetailsModal.js
+++ client/components/taskDetailsModal.js
@@ -4,22 +4,74 @@ export default function TaskDetailsModal({ isOpen, onClose, task, editTask }) {
   if (!isOpen) return null;
 
   return (
-    <div className='modal'>
-      <div className='modal-content'>
-        <div className='taskPopup'>
+    <div className="modal">
+      <div className="modal-content">
+        <div className="taskPopup">
           <h2>{task.Task_Name}</h2>
-          <button className='taskPopupButton'  onClick={() => editTask(task.Task_Name)}>Edit</button>
+          <button
+            className="taskPopupButton"
+            onClick={() => editTask(task.Task_Name)}
+          >
+            Edit
+          </button>
         </div>
-        
-        <div className='popContent'>
-          <strong>Assignee: <span className='content'>{task.Assignee.join(', ')}</span></strong>
-          <strong>Due Date: <span className='content'>{task.Due_Date}</span></strong>
-          <strong>Priority: <span className='content'>{task.Priority}</span></strong> 
-          <strong>Status: <span className='content'>{task.Status}</span></strong>
-          <strong>Description: <span className='content'>{task.Description}</span></strong>
-          <strong>Category: <span className='content'>{task.Category}</span></strong> 
+
+        <div className="popContent">
+          <div contentEditable="true">
+            <table>
+              <tr>
+                <td> Assignee:</td>
+                <td>{task.Assignee.join(', ')}</td>
+                <td>Due Date: </td>
+                <td>{task.Due_Date}</td>
+              </tr>
+              <tr>
+                <td>Category: </td>
+                <td>{task.Category}</td>
+                <td>Status: </td>
+                <td>{task.Status}</td>
+              </tr>
+              <tr>
+                <td>Priority: </td>
+                <td>{task.Priority}</td>
+                <td>Description:</td>
+                <td>{task.Description}</td>
+              </tr>
+            </table>
+
+            {/* <strong>
+              <br />
+              <span className="content"></span>
+            </strong>
+            <br />
+            <strong>
+              Due Date: <span className="content">{task.Due_Date}</span>
+            </strong>
+            <br />
+            <strong>
+              Priority: <span className="content">{task.Priority}</span>
+            </strong>
+            <br />
+            <strong>
+              Status: <span className="content">{task.Status}</span>
+            </strong>
+            <br />
+            <strong>
+              Description: <span className="content">{task.Description}</span>
+            </strong>
+            <br />
+            <strong>
+              Category: <span className="content">{task.Category}</span>
+            </strong>
+            <br /> */}
+          </div>
         </div>
-        <button className='popCloseButton' onClick={onClose}>Close</button>
+        <button
+          className="popCloseButton"
+          onClick={onClose}
+        >
+          Close
+        </button>
       </div>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -9,19 +9,16 @@ const app = express();
 
 // connect with mongoose database
 mongoose
-  .connect(
-    'mongodb+srv://connorelikeyes:D36U8CGSL5maEh9h@cluster0.vxtr5rx.mongodb.net/?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    } 
-  )
-  .then(() => {
-    console.log('MongoDB connected successfully');
-  })
-  .catch((err) => {
-    console.error('MongoDB connection error:', err);
-  });
+	.connect('mongodb+srv://connorelikeyes:D36U8CGSL5maEh9h@cluster0.vxtr5rx.mongodb.net/?retryWrites=true&w=majority', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log('MongoDB connected successfully');
+	})
+	.catch((err) => {
+		console.error('MongoDB connection error:', err);
+	});
 
 //handles parsing data & allows form data requests
 app.use(cors());
@@ -30,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 
 //statically serve everything in dist folder on static call
 app.use(express.static(path.join(__dirname, '../dist')));
-app.use('/stylesheets', express.static(path.join(__dirname, '../client/stylesheets'))); 
+app.use('/stylesheets', express.static(path.join(__dirname, '../client/stylesheets')));
 
 //Router to serve middleware & response
 app.use('/route', router);
@@ -38,14 +35,14 @@ app.use('/route', router);
 //Global error handler
 app.use((req, res) => res.status(404).send('Status Code 404: Page not found...'));
 app.use((err, req, res, next) => {
-  const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
-    status: 500,
-    message: { err: 'An error occurred in global error handler' },
-  };
-  const errorObj = Object.assign({}, defaultErr, err);
-  console.log(errorObj.log);
-  return res.status(errorObj.status).json(errorObj.message);
+	const defaultErr = {
+		log: 'Express error handler caught unknown middleware error',
+		status: 500,
+		message: { err: 'An error occurred in global error handler' },
+	};
+	const errorObj = Object.assign({}, defaultErr, err);
+	console.log(errorObj.log);
+	return res.status(errorObj.status).json(errorObj.message);
 });
 
 module.exports = app.listen(PORT, () => console.log('Listening in on PORT: ', PORT));
