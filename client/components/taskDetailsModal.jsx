be5b7c286f138307ac82f13b16ef392bbb6b4c23
Andrew <apwicker@gmail.com>
2023-09-20 12:19:32 -0700

    Drop Down Selection for Category is Populated with Categories from DB

--- client/components/taskDetailsModal.jsx
+++ client/components/taskDetailsModal.jsx
@@ -1,15 +1,35 @@
 import React, { useState, useEffect } from 'react';
 
+const dbURI = 'http://localhost:3000/route/category';
+
 //POPUP OF TASK ON CLICK OF TASK
 export default function TaskDetailsModal({ isOpen, onClose, task, editTask }) {
   if (!isOpen) return null;
   const [editedTask, setEditedTask] = useState({});
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     if (isOpen && task) {
       setEditedTask(task);
     }
   }, [isOpen, task]);
+
+  useEffect(() => {
+    fetch(dbURI)
+      .then((res) => res.json())
+      .then((data) => {
+        setCategories((prevCategories) => {
+          return data.map((el) => el.category).concat(prevCategories);
+        });
+      });
+  }, []);
+
+  console.log('the categories loaded in taskDetailsModal are: ', categories);
+  // console.log('the 0th element in categories is: ', categories[0]);
+  // console.log(
+  //   'the 0th element of the 0th element in categories is: ',
+  //   categories[0][0]
+  // );
 
   const handleFieldChange = (field, value) => {
     setEditedTask({
@@ -45,11 +65,19 @@ export default function TaskDetailsModal({ isOpen, onClose, task, editTask }) {
 
             <label>
               Category:
-              <input
-                type="text"
+              <select
                 value={editedTask.Category || ''}
                 onChange={(e) => handleFieldChange('Category', e.target.value)}
-              />
+              >
+                {categories.map((category, index) => (
+                  <option
+                    key={index}
+                    value={category}
+                  >
+                    {category}
+                  </option>
+                ))}
+              </select>
             </label>
             <br />
             <label>
