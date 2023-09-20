ca157282f2b77f22371c6069ae6651ba499ac62d
Andrew <apwicker@gmail.com>
2023-09-19 19:36:21 -0700

    task edit

--- client/components/taskDetailsModal.jsx
+++ client/components/taskDetailsModal.jsx
@@ -1,59 +1,46 @@
 import React from 'react';
 //POPUP OF TASK ON CLICK OF TASK
 export default function TaskDetailsModal({ isOpen, onClose, task, editTask }) {
-  if (!isOpen) return null;
+	if (!isOpen) return null;
 
-  function makeEditable(element) {
-    // const text = element.textContent;
-    // const input = document.createElement('input');
-    // input.type = 'text';
-    // input.value = text;
-    
-    // input.addEventListener('blur', function () {
-    //   const newText = input.value;
-    //   element.textContent = newText;
-    //   element.removeChild(input);
-    // });
+	return (
+		<div className='modal'>
+			<div className='modal-content'>
+				<div className='taskPopup'>
+					<h2>{task.Task_Name}</h2>
+					<button className='taskPopupButton' onClick={() => editTask(task.Task_Name)}>
+						Edit
+					</button>
+				</div>
 
-    // element.textContent = '';
-    // element.appendChild(input);
-    // input.focus();
-  }
-
-
-  return (
-    <div className='modal'>
-      <div className='modal-content'>
-        <div className='taskPopup'>
-          <h2>{task.Task_Name}</h2>
-
-
-          <button className='taskPopupButton'  onClick={() => editTask(
-            task._id,
-            // { Task_Name:'',
-            //   Assignee:'',
-            //   Due_Date:'',
-            //   Priority:'',
-            //   Status:'',
-            //   Description:'',
-            //   Category:'',}
-          )}>Edit</button>
-        </div>
-        
-        <div className='popContent'>
-            <label htmlFor="updateTaskInfo">Update Take Info</label>
-            <div className="editable" id="editableText" onClick={makeEditable(this)}>
-            Click here to edit this text.
-            </div>
-          {/* <strong>Assignee: <span className='content'>{task.Assignee.join(', ')}</span></strong>
-          <strong>Due Date: <span className='content'>{task.Due_Date}</span></strong>
-          <strong>Priority: <span className='content'>{task.Priority}</span></strong> 
-          <strong>Status: <span className='content'>{task.Status}</span></strong>
-          <strong>Description: <span className='content'>{task.Description}</span></strong>
-          <strong>Category: <span className='content'>{task.Category}</span></strong>  */}
-        </div>
-        <button className='popCloseButton' onClick={onClose}>Close</button>
-      </div>
-    </div>
-  );
+				<div className='popContent'>
+					<div contentEditable='true'>
+						<table>
+							<tr>
+								<td> Assignee:</td>
+								<td>{task.Assignee.join(', ')}</td>
+								<td>Due Date: </td>
+								<td>{task.Due_Date}</td>
+							</tr>
+							<tr>
+								<td>Category: </td>
+								<td>{task.Category}</td>
+								<td>Status: </td>
+								<td>{task.Status}</td>
+							</tr>
+							<tr>
+								<td>Priority: </td>
+								<td>{task.Priority}</td>
+								<td>Description:</td>
+								<td>{task.Description}</td>
+							</tr>
+						</table>
+					</div>
+				</div>
+				<button className='popCloseButton' onClick={onClose}>
+					Close
+				</button>
+			</div>
+		</div>
+	);
 }
