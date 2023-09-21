4029c4367c5e69d5a1501590ec051e14e7637cd9
Andrew <apwicker@gmail.com>
2023-09-21 12:10:48 -0700

    Update onDragEndLogic.js with helper functions for array reordering, updating task category in the backend, and deleting and replacing a task.

--- client/components/onDragEndLogic.js
+++ client/components/onDragEndLogic.js
@@ -1,71 +1,5 @@
 const dbURI = 'http://localhost:3000/route';
 
-export const onDragEnd = (
-  result,
-  categories,
-  setCategories,
-  users,
-  setUsers,
-  setActiveIndex,
-  setActiveDroppableId
-) => {
-  const { source, destination } = result;
-  console.log(
-    `the source is ${JSON.stringify(
-      source
-    )}, and the destination is ${JSON.stringify(destination)}`
-  );
-  // If dropped out of bounds, end the function (and return item to starting point)
-  if (!destination) return;
-
-  // Declare a boolean variable set to the output of checking if droppable id is userCategory
-  const isUserCategory = source.droppableId === 'userCategory';
-
-  // Declare a boolean variable set to the output of checking if droppable id is same as destination id
-  const isSameCategory = source.droppableId === destination.droppableId;
-
-  const sourceCategory = categories[source.droppableId];
-  console.log('The source category is: ', sourceCategory);
-  const destCategory = categories[destination.droppableId];
-  console.log('The dest category is: ', destCategory);
-  const sourceItems = sourceCategory?.items ? [...sourceCategory.items] : [];
-  console.log('The source items are: ', sourceItems);
-  const destItems = destCategory?.items ? [...destCategory.items] : [];
-  console.log('The dest items are: ', destItems);
-
-  const [removed] = sourceItems.splice(source.index, 1);
-
-  if (isSameCategory) {
-    sourceItems.splice(destination.index, 0, removed);
-    setCategories({
-      ...categories,
-      [source.droppableId]: { ...sourceCategory, items: sourceItems },
-    });
-    console.log('same category!');
-    return;
-  } else {
-    if (destItems.length > destination.index) {
-      destItems.splice(destination.index, 1);
-    }
-
-    destItems.splice(destination.index, 0, removed);
-    if (Object.prototype.hasOwnProperty.call(removed, 'Category')) {
-      removed.Category = destination.droppableId;
-    }
-
-    console.log('different category!');
-    setCategories({
-      ...categories,
-      [source.droppableId]: { ...sourceCategory, items: sourceItems },
-      [destination.droppableId]: { ...destCategory, items: destItems },
-    });
-  }
-
-  setActiveIndex(null);
-  setActiveDroppableId(null);
-  updateTaskInDatabase(removed);
-};
-
 // Helper function to reorder an array based on the start and end indices
 const reorderArray = (array, startIndex, endIndex) => {
   // Cache variable to store the element being moved (at startIndex)
@@ -94,4 +28,86 @@ const updateTaskInDatabase = (updatedTask) => {
         error
       );
     });
+};
+
+const deleteTaskFromDatabase = (taskId) => {
+  fetch(`${dbURI}/task/${taskId}`, {
+    method: 'DELETE',
+  })
+    .then((res) => res.json())
+    .then(() => {
+      alert('Task successfully deleted');
+    })
+    .catch((error) => {
+      console.error(
+        'deleteTaskFromDatabase in onDragEndLogic suffered and error trying to delete task: ',
+        error
+      );
+    });
+};
+
+// Helper function to delete and replace a task
+const deleteAndReplaceTask = (destItems, destination, removed) => {
+  const itemToDelete = destItems[destination.index];
+  const isConfirmed = window.confirm(
+    'Are you sure you want to replace this task? The existing task will be deleted!'
+  );
+  if (isConfirmed) {
+    destItems.splice(destination.index, 1);
+    deleteTaskFromDatabase(itemToDelete._id);
+    destItems.splice(destination.index, 0, removed);
+  }
+};
+
+export const onDragEnd = (
+  result,
+  categories,
+  setCategories,
+  users,
+  setUsers,
+  setActiveIndex,
+  setActiveDroppableId
+) => {
+  const { source, destination } = result;
+
+  // If dropped out of bounds, end the function (and return item to starting point)
+  if (!destination) return;
+
+  // Declare a boolean variable set to the output of checking if droppable id is userCategory
+  const isUserCategory = source.droppableId === 'userCategory';
+  // Declare a boolean variable set to the output of checking if droppable id is same as destination id
+  const isSameCategory = source.droppableId === destination.droppableId;
+
+  const sourceCategory = categories[source.droppableId];
+  const destCategory = categories[destination.droppableId];
+  const sourceItems = [...(sourceCategory?.items || [])];
+  const destItems = [...(destCategory?.items || [])];
+
+  const [removed] = sourceItems.splice(source.index, 1);
+
+  if (isSameCategory) {
+    sourceItems.splice(destination.index, 0, removed);
+    setCategories({
+      ...categories,
+      [source.droppableId]: { ...sourceCategory, items: sourceItems },
+    });
+  } else {
+    if (destItems.length > destination.index) {
+      deleteAndReplaceTask(destItems, destination, removed);
+    } else {
+      destItems.splice(destination.index, 0, removed);
+    }
+    if (Object.prototype.hasOwnProperty.call(removed, 'Category')) {
+      removed.Category = destination.droppableId;
+    }
+    setCategories({
+      ...categories,
+      [source.droppableId]: { ...sourceCategory, items: sourceItems },
+      [destination.droppableId]: { ...destCategory, items: destItems },
+    });
+  }
+
+  setActiveIndex(null);
+  setActiveDroppableId(null);
+  updateTaskInDatabase(removed);
 };
